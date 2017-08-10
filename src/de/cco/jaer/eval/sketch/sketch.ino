37f646ed9fa2814a5a07717e095bf001fd4f85ac
Viktor Bahr <viktor@bccn-berlin.de>
2017-08-10 12:09:29 +0200

    Minor changes regarding arduino config.

--- src/de/cco/jaer/eval/sketch/sketch.ino
+++ src/de/cco/jaer/eval/sketch/sketch.ino
@@ -1,14 +1,19 @@
 
+const int YELLOW = 13;
+const int RED = 12;
+const int GREEN = 11;
 const int LED = 13; // LED connected to digital pin 13
 const int SYNC_OFF = 49; // jAER logging stopped
 const int SYNC_ON = 50; // jAER logging started
-const int LASER_ON = 101; // swtich on optogenetic laser
-const int LASER_OFF = 102; // switch off optogenetic laser
+const int LASER_ON = 65; // swtich on optogenetic laser
+const int LASER_OFF = 66; // switch off optogenetic laser
 int recv = 0; // byte received on the serial port
 
 void setup() {
   // initialize onboard LED (led) and serial port
-  pinMode(LED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
   Serial.begin(9600);
   Serial.println("--Arduino ready.");
 }
@@ -19,24 +24,31 @@ void loop()
   if (Serial.available() > 0) {
     recv = Serial.read();
 
-    // if '0' (decimal 48) is received, turn SNYC on
-    // if '1' (decimal 49) is received, turn SYNC off
+    // if '1' (decimal 49) is received, turn SNYC on
+    // if '2' (decimal 50) is received, turn SYNC off
     // if 'A' (decimal 101) is received, turn LED on
     // if 'B', or any other non registered character is received, turn LED off
     switch (recv) 
     {
       case LASER_ON:
-        digitalWrite(LED, HIGH);
+        digitalWrite(GREEN, HIGH);
+        digitalWrite(RED, LOW);
+        digitalWrite(YELLOW, LOW);
         break;
       case LASER_OFF: 
-        digitalWrite(LED, LOW);
+        digitalWrite(GREEN, LOW);
+        digitalWrite(RED, HIGH);
+        digitalWrite(YELLOW, LOW);
         break;
       case SYNC_ON:
+        digitalWrite(GREEN, LOW);
+        digitalWrite(RED, LOW);
+        digitalWrite(YELLOW, HIGH);
         break;
       case SYNC_OFF:
-        break;
-      default:
-        digitalWrite(LED, LOW);
+        digitalWrite(GREEN, LOW);
+        digitalWrite(RED, LOW);
+        digitalWrite(YELLOW, LOW);
         break;
     }
 
