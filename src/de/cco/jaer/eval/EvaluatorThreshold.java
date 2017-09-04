254e9a30374f54f44d4dce92945e1d5351829783
Viktor Bahr <viktor@bccn-berlin.de>
2017-09-04 18:07:39 +0200

    Added threshold class as a connection between evaluator and user interface.

---
+++ src/de/cco/jaer/eval/EvaluatorThreshold.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cco.jaer.eval;
+
+import java.util.EnumMap;
+
+/**
+ *
+ * @author viktor
+ */
+public class EvaluatorThreshold<T> {
+ 
+    public enum Parameter {
+        EVENTRATE,
+        POSITION,
+        DISTANCE,
+        SPEED
+    }
+    
+    public EnumMap<Parameter,String> paramType = new EnumMap<>(Parameter.class);
+    
+    private Parameter param;
+    private T value;
+    
+    public EvaluatorThreshold( Parameter param, T value ) {
+        initTypeMap();
+        setTarget(param);
+        setValue(value);
+    }
+    
+    public T getValue() {
+        return this.value;
+    }
+    
+    public Parameter getTarget() {
+        return this.param;
+    }
+    
+    public void setValue(T value) {
+        String type = value.getClass().getName();
+        if (paramType.get(getTarget()).equals(type)) { this.value = null; }
+        this.value = value;
+    }
+    
+    private void setTarget(Parameter param) {
+        this.param = param;
+    }
+    
+    private void initTypeMap() {
+        paramType.put(Parameter.EVENTRATE, "java.lang.Double");
+        paramType.put(Parameter.POSITION, "int[]");
+        paramType.put(Parameter.DISTANCE, "java.lang.Double");
+        paramType.put(Parameter.SPEED, "java.lang.Double");
+    }
+    
+}
